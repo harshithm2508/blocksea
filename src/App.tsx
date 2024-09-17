0fbfd1692cc6bc7d615e4dbdcf90aaeb7e318784
harshithm2508 <harshithm2508@gmail.com>
2024-09-17 10:11:36 +0530

    got recent mined blocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Navbar from "../src/components/Navbar.tsx"
 
 function App(){
   return(
     <BrowserRouter>
+      <Navbar/>      
       <Routes>
         <Route path="/" element={<Dashboard/>}/>
       </Routes>
---
+++ src/state/atoms/BlockState.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const recentBlock = atom({
+    key : "RecentBlockNumber",
+    default : ''
+})
