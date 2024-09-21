0dc59780a32b2286b928bb35a677bde07c6d582b
harshithm2508 <harshithm2508@gmail.com>
2024-09-21 14:01:40 +0530

    sigin button update, gas price

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,6 @@ function AboveNavbar(){
         axios
         .request(options)
         .then(function (response : any) {
-            console.log(response.data);
             setEthPrice(response.data.ethereum.usd)
             setEthPriceChange(Math.round(response.data.ethereum.usd_24h_change * 100) / 100)
             setBtcPrice(response.data.bitcoin.usd);
@@ -104,12 +103,15 @@ function BelowNavbar() {
 
 
                 {/* // Avatar Sign In Part  */}
-                <li className=" flex items-center gap-2 px-2 border-l pl-3">
+                <li className=" flex items-center gap-2 px-2 border-l pl-3 hover:text-blue-300 cursor-pointer">
                         <div className="relative w-6 h-6 overflow-hidden bg-gray-700 rounded-full">
-                            <svg className="absolute w-7 h-7 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"></path></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+
                         </div>
                     <Link to={'/signin'}>
-                        <div className=" hover:text-blue-300">Sign In</div>
+                        <div className=" ">Sign In</div>
                     </Link>
                 </li>
             </ul>
--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,6 +1,6 @@
 import { useRecoilState, useRecoilValue } from "recoil";
 import { btcPrice, etherPrice, recentBlock } from "../state/atoms/BlockState";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { publicClient } from "../clients/publicClient";
 
 function Statistics(){
@@ -8,12 +8,19 @@ function Statistics(){
     const ethPrice = useRecoilValue(etherPrice)
     const bitcoinPrice = useRecoilValue(btcPrice);
     const [recentlyMinedBlock, setRecentlyMinedBlock ] = useRecoilState(recentBlock);
+    const [ currentGasPrice, setCurrentGasPrice ] = useState<BigInt>(BigInt(0));
 
     useEffect(() => {
         const getRecentlyMinedBlock = async () => {
             const block = await publicClient.getBlockNumber();
             setRecentlyMinedBlock(block.toString());
         };
+
+        const getGasPrice = async () => {
+            const gas : bigint = await publicClient.getGasPrice();
+            setCurrentGasPrice(gas);
+        }
+        getGasPrice();
 
         const intervalId = setInterval(() => {
             console.log("Set Interval run ")
@@ -45,13 +52,32 @@ function Statistics(){
 
                 {/* //  Recently Mined Block  */}
                 <div className=" p-4 border-r-2 border-b-2 flex flex-col text-gray-50">
-                    <div className=" text-gray-400 text-xs tracking-widest">RECENTLY MINED BLOCK </div>
-                    <div>{recentlyMinedBlock}</div>
+                    <div className=" flex gap-4">
+                        <div className=" flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"        className="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m7.875 14.25 1.214 1.942a2.25 2.25 0 0 0 1.908 1.058h2.006c.776 0 1.497-.4 1.908-1.058l1.214-1.942M2.41 9h4.636a2.25 2.25 0 0 1 1.872 1.002l.164.246a2.25 2.25 0 0 0 1.872 1.002h2.092a2.25 2.25 0 0 0 1.872-1.002l.164-.246A2.25 2.25 0 0 1 16.954 9h4.636M2.41 9a2.25 2.25 0 0 0-.16.832V12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 12V9.832c0-.287-.055-.57-.16-.832M2.41 9a2.25 2.25 0 0 1 .382-.632l3.285-3.832a2.25 2.25 0 0 1 1.708-.786h8.43c.657 0 1.281.287 1.709.786l3.284 3.832c.163.19.291.404.382.632M4.5 20.25h15A2.25 2.25 0 0 0 21.75 18v-2.625c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125V18a2.25 2.25 0 0 0 2.25 2.25Z" />
+                            </svg>
+                        </div>
+
+                        <div className=" flex flex-col">
+                            <div className=" text-gray-400 text-xs tracking-widest">RECENTLY MINED BLOCK </div>
+                            <div>{recentlyMinedBlock}</div>
+                        </div>
+                    </div>
+                    
                 </div>
 
                 
                 <div className=" p-2 border-b-2 row-span-2">d</div>
-                <div className=" p-2 border-r-2">d</div>
+
+                {/* //  Current price of gas  */}
+                <div className=" p-2 border-r-2 text-white">
+                    <div></div>
+                    <div className=" flex flex-col gap-1 justify-center">
+                        <div className="  text-xs text-gray-400">CURRENT GAS PRICE</div>
+                        <div>{currentGasPrice.toString()}</div>
+                    </div>
+                </div>
                 <div className=" p-2 border-r-2">d</div>
             </div>
         </div>
