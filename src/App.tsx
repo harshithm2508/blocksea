2a768d43abd22f1f6d2ee418229c570060a24b36
harshithm2508 <harshithm2508@gmail.com>
2024-09-24 20:41:30 +0530

    update RecoilRoot position

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Navbar from "../src/components/Navbar.tsx"
-import ThemeProvider from "./components/ThemeProvider.tsx";
-import { RecoilRoot } from "recoil";
 
 function App(){
   return(
-    <RecoilRoot>
-      <ThemeProvider>
-        <PageRoutes/>
-      </ThemeProvider>
-    </RecoilRoot>
-  )
-}
-
-
-function PageRoutes(){
-  return(
-    <BrowserRouter>
-      <Navbar/>      
-      <Routes>
-        <Route path="/" element={<Dashboard/>}/>
-      </Routes>
-    </BrowserRouter>
+        <BrowserRouter>
+          <Navbar/>      
+          <Routes>
+            <Route path="/" element={<Dashboard/>}/>
+          </Routes>
+        </BrowserRouter>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { StrictMode } from 'react'
+import { RecoilRoot } from 'recoil'
+import ThemeProvider from './components/ThemeProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <StrictMode>
+    <ThemeProvider>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </ThemeProvider>
+  </StrictMode>
 )
